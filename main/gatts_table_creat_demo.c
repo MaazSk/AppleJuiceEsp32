06d3482babae861840a77445bbf7b4f1df9915a1
MaazSk <maazshaikh05@gmail.com>
2023-11-08 00:23:52 +0530

    Added random Mac generation and updated the database

--- main/gatts_table_creat_demo.c
+++ main/gatts_table_creat_demo.c
@@ -27,6 +27,8 @@
 #include "gatts_table_creat_demo.h"
 #include "esp_gatt_common_api.h"
 
+#include "esp_random.h"
+
 #define GATTS_TABLE_TAG "APPLEJUICE"
 
 #define PROFILE_NUM                 1
@@ -50,7 +52,7 @@ static uint8_t adv_config_done       = 0;
 uint16_t heart_rate_handle_table[HRS_IDX_NB];
 
 
-#define CONFIG_SET_RAW_ADV_DATA 
+// #define CONFIG_SET_RAW_ADV_DATA 
 #ifdef CONFIG_SET_RAW_ADV_DATA
 static uint8_t raw_adv_data[] = {
 0x02,       0x01,       0x18,
@@ -63,14 +65,6 @@ static uint8_t _raw_adv_data[] = {
     0x06,       0x16,       0x2c,   0xFE,   0x92,   0xBB,   0xBD,
     0x02,       0x0a,       0xeb,
     0x05,       0x09,       'B','u','d','s'
-        // /* flags */
-        // 0x02, 0x01, 0x06,
-        // /* tx power*/
-        // 0x02, 0x0a, 0xeb,
-        // /* service uuid */
-        // 0x03, 0x03, 0xFF, 0x00,
-        // /* device name */
-        // 0x0f, 0x09, 'E', 'S', 'P', '_', 'G', 'A', 'T', 'T', 'S', '_', 'D','E', 'M', 'O'
 };
 #else
 static uint8_t service_uuid[16] = {
@@ -79,12 +73,30 @@ static uint8_t service_uuid[16] = {
     0xfb, 0x34, 0x9b, 0x5f, 0x80, 0x00, 0x00, 0x80, 0x00, 0x10, 0x00, 0x00, 0x2C, 0xFE, 0x00, 0x00,
 };
 static uint8_t service_data[] = {0x2C,0xFE,0x92,0xBB,0xBD};
+static uint8_t service_data_1[] = {0x2C,0xFE,0xCD,0x82,0x56};
+static uint8_t service_data_2[] = {0x2C,0xFE,0x00,0x00,0xF0};
+static uint8_t service_data_3[] = {0x2C,0xFE,0xF0,0x00,0x00};
+static uint8_t service_data_4[] = {0x2C,0xFE,0x82,0x1F,0x66};
+static uint8_t service_data_5[] = {0x2C,0xFE,0xF5,0x24,0x94};
+static uint8_t service_data_6[] ={0x2C,0xFE,0x00,0x30,0x00};
+static uint8_t service_data_7[] ={0x2C,0xFE,0x00,0x30,0x01};
+static uint8_t service_data_8[] ={0x2C,0xFE,0x00,0xA1,0x68};
+static uint8_t service_data_9[] ={0x2C,0xFE,0x00,0xAA,0x48};
+static uint8_t service_data_10[] = {0x2C,0xFE,0x00,0xAA,0x91};
+static uint8_t service_data_11[] ={0x2C,0xFE,0x00,0xC9,0x5C};
+
+uint8_t *arrays[] = {service_data, service_data_1, service_data_2, service_data_3, service_data_4, service_data_5,service_data_6,
+service_data_7,
+service_data_8,
+service_data_9,
+service_data_10,
+service_data_11};
 /* The length of adv data must be less than 31 bytes */
 static esp_ble_adv_data_t adv_data = {
-    .include_name = true, // Only include this if necessary
+    .include_name = false, // Only include this if necessary
     .include_txpower = true,
     .service_data_len = sizeof(service_data),
-    .p_service_data = service_data,
+    .p_service_data = service_data_5,
     .service_uuid_len = sizeof(service_uuid),
     .p_service_uuid = service_uuid,
 };
@@ -93,11 +105,12 @@ static esp_ble_adv_data_t adv_data = {
 static esp_ble_adv_params_t adv_params = {
     .adv_int_min         = 0x20,
     .adv_int_max         = 0x40,
-    .adv_type            = ADV_TYPE_IND,
-    .own_addr_type       = BLE_ADDR_TYPE_PUBLIC,
+    .adv_type            = ADV_TYPE_IND,//ADV_TYPE_DIRECT_IND_HIGH,//ADV_TYPE_NONCONN_IND,//ADV_TYPE_SCAN_IND,//ADV_TYPE_IND,
+    .own_addr_type       = BLE_ADDR_TYPE_RANDOM,//BLE_ADDR_TYPE_PUBLIC,
     .channel_map         = ADV_CHNL_ALL,
     .adv_filter_policy   = ADV_FILTER_ALLOW_SCAN_ANY_CON_ANY,
 };
+esp_bd_addr_t dummy_addr = {0xDE, 0xAD, 0xBE, 0xEF, 0xBA, 0xBE};
 
 struct gatts_profile_inst {
     esp_gatts_cb_t gatts_cb;
@@ -194,6 +207,10 @@ static void gatts_profile_event_handler(esp_gatts_cb_event_t event, esp_gatt_if_
             esp_err_t set_dev_name_ret = esp_ble_gap_set_device_name(SAMPLE_DEVICE_NAME);
             if (set_dev_name_ret){
                 ESP_LOGE(GATTS_TABLE_TAG, "set device name failed, error code = %x", set_dev_name_ret);
+            }
+           set_dev_name_ret=  esp_ble_gap_set_rand_addr(dummy_addr);
+           if (set_dev_name_ret){
+                ESP_LOGE(GATTS_TABLE_TAG, "Address error,error code = %x", set_dev_name_ret);
             }
     #ifdef CONFIG_SET_RAW_ADV_DATA
             esp_err_t raw_adv_ret = esp_ble_gap_config_adv_data_raw(raw_adv_data, sizeof(raw_adv_data));
@@ -310,6 +327,8 @@ static void gatts_event_handler(esp_gatts_cb_event_t event, esp_gatt_if_t gatts_
     } while (0);
 }
 
+void randomDevices();
+
 void app_main(void)
 {
     esp_err_t ret;
@@ -321,6 +340,7 @@ void app_main(void)
         ret = nvs_flash_init();
     }
     ESP_ERROR_CHECK( ret );
+
 
     ESP_ERROR_CHECK(esp_bt_controller_mem_release(ESP_BT_MODE_CLASSIC_BT));
 
@@ -371,4 +391,110 @@ void app_main(void)
     if (local_mtu_ret){
         ESP_LOGE(GATTS_TABLE_TAG, "set local  MTU failed, error code = %x", local_mtu_ret);
     }
+    xTaskCreate(randomDevices,"BLE",4096,NULL,2,NULL);
+}
+
+void changeData(int _data)
+{
+switch (_data)
+{
+    esp_err_t set_dev_name_ret;
+case 0:
+    adv_data.p_service_data = service_data_1;
+     set_dev_name_ret = esp_ble_gap_set_device_name("Bose NC 700");
+            if (set_dev_name_ret){
+                ESP_LOGE(GATTS_TABLE_TAG, "set device name failed, error code = %x", set_dev_name_ret);
+            }
+    /* code */
+    break;
+case 1:
+    adv_data.p_service_data = service_data_2;
+     set_dev_name_ret = esp_ble_gap_set_device_name("Bose QuietComfort");
+            if (set_dev_name_ret){
+                ESP_LOGE(GATTS_TABLE_TAG, "set device name failed, error code = %x", set_dev_name_ret);
+            }
+    /* code */
+    break;
+    case 2:
+    adv_data.p_service_data = service_data_3;
+     set_dev_name_ret = esp_ble_gap_set_device_name("JBL Flip 6");
+            if (set_dev_name_ret){
+                ESP_LOGE(GATTS_TABLE_TAG, "set device name failed, error code = %x", set_dev_name_ret);
+            }
+    
+    /* code */
+    break;
+    case 3:
+    adv_data.p_service_data = service_data_4;
+    adv_data.p_service_data = service_data_3;
+     set_dev_name_ret = esp_ble_gap_set_device_name("JBL PRO 6");
+            if (set_dev_name_ret){
+                ESP_LOGE(GATTS_TABLE_TAG, "set device name failed, error code = %x", set_dev_name_ret);
+            }
+    
+    /* code */
+    break;
+
+default:
+adv_data.p_service_data = service_data;
+// adv_data.p_service_data = service_data_3;
+     set_dev_name_ret = esp_ble_gap_set_device_name("Buds");
+            if (set_dev_name_ret){
+                ESP_LOGE(GATTS_TABLE_TAG, "set device name failed, error code = %x", set_dev_name_ret);
+            }
+    break;
+}
+}
+
+void randomGenerator()
+{
+    for (int i = 0; i < 6; i++){
+    dummy_addr[i] = esp_random();
+
+    // It seems for some reason first 4 bits
+    // Need to be high (aka 0b1111), so we 
+    // OR with 0xF0
+    if (i == 0){
+      dummy_addr[i] |= 0xF0;
+    }
+  }
+}
+
+void randomDevices()
+{
+    vTaskDelay(pdMS_TO_TICKS(5000));
+    int i = 0;
+    while(1)
+    {
+        i = 0;
+        for(i = 0; i < 11; i++)
+        {
+            uint32_t random_value;
+    esp_fill_random(&random_value, sizeof(random_value));
+    uint8_t random_number = random_value % 12; // Modulo 12 for a number between 0 and 11
+    ESP_LOGE(GATTS_TABLE_TAG,"RAndom number %d",random_number);
+    
+    esp_ble_gap_stop_advertising(/*&adv_params*/);
+    randomGenerator();
+    esp_err_t set_dev_name_ret=  esp_ble_gap_set_rand_addr(dummy_addr);
+           if (set_dev_name_ret){
+                ESP_LOGE(GATTS_TABLE_TAG, "Address error,error code = %x", set_dev_name_ret);
+            }
+    // changeData(i);
+    adv_data.p_service_data = arrays[random_number];
+    esp_err_t ret = esp_ble_gap_config_adv_data(&adv_data);
+            if (ret){
+                ESP_LOGE(GATTS_TABLE_TAG, "config adv data failed, error code = %x", ret);
+            }
+             ret = esp_ble_gap_start_advertising(&adv_params);
+            if (ret){
+                ESP_LOGE(GATTS_TABLE_TAG, "config adv data failed, error code = %x", ret);
+            }
+            vTaskDelay(pdMS_TO_TICKS(1000));
+            for(int j = 0; j < 6;j++)
+                printf("0x%X\t",dummy_addr[j]);
+            printf("\r\n");
+        }
+    }
+
 }
